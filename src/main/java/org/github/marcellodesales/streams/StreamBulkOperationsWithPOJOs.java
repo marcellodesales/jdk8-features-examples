7b47e1141ded2c12547387be7801ba0329b0fe79
Marcello de Sales <Marcello_DeSales@intuit.com>
2014-02-21 23:46:32 -0800

    Fixing the example of StringBulkOperationsWithPOJOS to properly group POJOs
    The example was not displaying the power of its implementation. The example
    will output the following:
    
    {de Sales=[[MarceLLo de Sales], [Leandro de Sales]], Maria=[[Livia Maria]]}
    
    This is very similar to a Map process because of the use of groupingBy().

--- src/main/java/org/github/marcellodesales/streams/StreamBulkOperationsWithPOJOs.java
+++ src/main/java/org/github/marcellodesales/streams/StreamBulkOperationsWithPOJOs.java
@@ -11,16 +11,16 @@ public class StreamBulkOperationsWithPOJOs {
   public static void main(String[] args) {
 
     List<Person> list = Arrays.asList(
-        new Person("Marcello", "de Sales"),
-        new Person("Thiago", "Bruno"),
-        new Person("Leandro", "Melo"),
+        new Person("MarceLLo", "de Sales"),
+        new Person("Thiago", "de Sales"),
+        new Person("Leandro", "de Sales"),
         new Person("Livia", "Maria"));
 
     // grouping
     System.out.println(
         list.stream()
-          .filter(p -> p.getLastName().length() < 7)
-          .collect(Collectors.groupingBy(Person::getFirstName)));
+          .filter(p -> p.getFirstName().contains("L"))
+          .collect(Collectors.groupingBy(Person::getLastName)));
 
   }
 
